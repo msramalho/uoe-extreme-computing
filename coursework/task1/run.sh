189dccaa3e47cb92d1eea412b38aedf03b6cf5b6
msramalho <up201403027@fe.up.pt>
2019-10-08 00:40:37 +0100

    updated run files

--- coursework/task1/run.sh
+++ coursework/task1/run.sh
@@ -1,14 +1,19 @@
-INPUT_DIR=/data/supplementary/title.basics.tsv
-OUTPUT_DIR=/user/$USER/data/output/labs/
+TASK=task1
+INPUT1=/data/small/imdb/title.basics.tsv
+OUTPUT_DIR=/user/$USER/data/output/$TASK/
 
 hdfs dfs -rm -r $OUTPUT_DIR
 
 hadoop jar /opt/hadoop/hadoop-2.9.2/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--input $INPUT_DIR \
+-input $INPUT1 \
 -output $OUTPUT_DIR \
 -file ./mapper.py \
 -mapper mapper.py \
 -file ./reducer.py \
 -reducer reducer.py
 
+echo "DONE, here is the output:"
+
 hdfs dfs -cat $OUTPUT_DIR/*
+
+echo "-----DONE-----"
---
+++ coursework/task2/run.sh
@@ -0,0 +1,25 @@
+TASK=task2
+INPUT1=/data/small/imdb/title.basics.tsv
+INPUT2=/data/small/imdb/title.ratings.tsv
+OUTPUT_DIR=/user/$USER/data/output/$TASK/
+
+hdfs dfs -rm -r $OUTPUT_DIR
+
+hadoop jar /opt/hadoop/hadoop-2.9.2/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
+-D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
+ -D mapreduce.map.output.key.field.separator="|" \
+ -D mapreduce.output.textoutputformat.separator="|" \
+ -D stream.map.output.field.separator="|" \
+-input $INPUT1 \
+-input $INPUT2 \
+-output $OUTPUT_DIR \
+-file ./mapper.py \
+-mapper mapper.py \
+-file ./reducer.py \
+-reducer reducer.py
+
+echo "DONE, here is the output:"
+
+hdfs dfs -cat $OUTPUT_DIR/*
+
+echo "-----DONE-----"
