84adf48d365d3da2f0e5747625cfa88b7ed8efd3
msramalho <up201403027@fe.up.pt>
2019-10-08 18:10:22 +0100

    fixed run.sh for task1

--- coursework/task1/run.sh
+++ coursework/task1/run.sh
@@ -6,11 +6,9 @@ hdfs dfs -rm -r $OUTPUT_DIR
 
 hadoop jar /opt/hadoop/hadoop-2.9.2/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -D mapred.job.name="Miguel's task 1 - s2004624" \
--D stream.map.output.field.separator="|" \
--D stream.reduce.input.field.separator="|" \
+-files ./mapper.py,./reducer.py \
 -input $INPUT1 \
 -output $OUTPUT_DIR \
--files ./mapper.py,./reducer.py \
 -mapper mapper.py \
 -reducer reducer.py
 
